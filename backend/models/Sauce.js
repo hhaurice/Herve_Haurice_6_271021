e154ce24150628b1ccd95193a8167709d2fafd08
Herve <herve.haurice@gmail.com>
2021-10-27 18:17:52 +0200

    Added Sauce.js to models directory + mongoose unique validator

---
+++ backend/models/Sauce.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const sauceSchema = mongoose.Schema(
+    {
+ userId:                { type: String, required: true, unique: true },
+ name:                  { type: String },
+ manufacturer:          { type: String },
+ description:           { type: String },
+ mainPepper:            { type: String },
+ imageUrl:              { type: String },
+ heat:                  { type: Number },
+ likes:                 { type: Number, default: 0 },
+ dislikes:              { type: Number, default: 0 },
+ usersLiked:            [{type: String}],
+ usersDisliked:         [{type: String}]
+});
+
+
+module.exports = mongoose.model("Sauce", sauceSchema);
