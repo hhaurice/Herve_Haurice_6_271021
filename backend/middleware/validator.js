1e855433458d3e4c783f2de5289030a132dd691d
Herve <herve.haurice@gmail.com>
2021-11-04 12:20:23 +0100

    Rewrote error in validator

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -2,7 +2,7 @@ const { body, validationResult } = require('express-validator')
 
 const passwordValidator = () => {
   return [
-    // password must be at least 5 chars long
+    // password must be at least 8 characters long
     body("password")
     .isStrongPassword({ 
         minLength: 8, 
@@ -10,7 +10,7 @@ const passwordValidator = () => {
         returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10, 
         pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 
     })
-    .withMessage("Mot de passe doit contenir au moins 8 caractères dont une majuscule, un symbole et un chiffre")
+    .withMessage("Mot de passe doit contenir au moins 8 caractères contenant une majuscule, un symbole et un chiffre")
   ]
 };
 
@@ -23,7 +23,7 @@ const validate = (req, res, next) => {
   const extractedErrors = []
   error.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
 
-  return res.status(422).json({errors: "Mot de passe doit contenir au moins 8 caractères contenant une majuscule, un symbole et un chiffre"})
+  return res.status(422).json({error: "Mot de passe doit contenir au moins 8 caractères contenant une majuscule, un symbole et un chiffre"})
 
 };
 
